81cbc2b4d7c9e1c48c46f8b1c941b7238ffceda1
dylan <1611185386@qq.com>
2024-02-26 21:03:25 +0800

    feat: 什么时候重渲染发生,如何防止重渲染

--- src/pages/best/RerenderBase.tsx
+++ src/pages/best/RerenderBase.tsx
@@ -1,11 +1,15 @@
-import { useState } from "react"
+import { memo, useState } from "react"
 import { Button } from "antd"
 const Child = () => {
   console.log("Child")
   return <div>子组件</div>
 }
+// 父组件更新,如果子组件不用memo包裹,子组件也会重渲染
+const MemoChild = memo(Child)
 const RerenderBase = () => {
   const [count, setCount] = useState(0)
+  console.log("父组件")
+  // 设置同一个state,组件会更新吗? 不会
   return (
     <div>
       <p>组件在什么时候会重渲染?</p>
@@ -17,8 +21,12 @@ const RerenderBase = () => {
         <Button type="primary" onClick={() => setCount(count + 1)}>
           加一
         </Button>
+        <Button type="default" onClick={() => setCount(count)}>
+          count不变
+        </Button>
       </div>
-      <Child></Child>
+      {/* <Child></Child> */}
+      <MemoChild></MemoChild>
     </div>
   )
 }
