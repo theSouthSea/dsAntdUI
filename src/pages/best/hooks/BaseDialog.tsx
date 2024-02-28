947838dafb9b8bffc511da2492a8c09b662d4056
dylan <1611185386@qq.com>
2024-02-28 17:59:01 +0800

    feat: 拆分成useScroll对组件重渲染没有影响

--- src/pages/best/hooks/BaseDialog.tsx
+++ src/pages/best/hooks/BaseDialog.tsx
@@ -39,15 +39,156 @@ const modalBlanketCss = css`
 //   ) : null
 // }
 /* 添加获取模态框滚动位置的功能 */
-export const ModalBase = React.forwardRef(
-  ({ isOpen, onClosed }: ModalProps, ref: RefObject<any>) => {
-    return isOpen ? (
-      <>
-        <div css={modalBlanketCss} onClick={onClosed} />
-        <div css={modalBodyCss} ref={ref}>
-          Modal dialog content
-        </div>
-      </>
-    ) : null
-  }
-)
+// export const ModalBase = React.forwardRef(
+//   ({ isOpen, onClosed }: ModalProps, ref: RefObject<any>) => {
+//     return isOpen ? (
+//       <>
+//         <div css={modalBlanketCss} onClick={onClosed} />
+//         <div css={modalBodyCss} ref={ref}>
+//           Modal dialog content
+//         </div>
+//       </>
+//     ) : null
+//   }
+// )
+export const ModalBase = React.forwardRef<any, ModalProps>(({ isOpen, onClosed }, ref) => {
+  return isOpen ? (
+    <>
+      <div css={modalBlanketCss} onClick={onClosed} />
+      <div css={modalBodyCss} ref={ref}>
+        <h3>Try React</h3>
+        <p>
+          React has been designed from the start for gradual adoption, and you can use as little or
+          as much React as you need. Whether you want to get a taste of React, add some
+          interactivity to a simple HTML page, or start a complex React-powered app, the links in
+          this section will help you get started.
+        </p>
+        <h3>Online Playgrounds</h3>
+        <p>
+          If you’re interested in playing around with React, you can use an online code playground.
+          Try a Hello World template on CodePen, CodeSandbox, or Stackblitz.
+        </p>
+        <p>
+          If you prefer to use your own text editor, you can also download this HTML file, edit it,
+          and open it from the local filesystem in your browser. It does a slow runtime code
+          transformation, so we’d only recommend using this for simple demos.
+        </p>
+        <h3>Add React to a Website</h3>
+        <p>
+          You can add React to an HTML page in one minute. You can then either gradually expand its
+          presence, or keep it contained to a few dynamic widgets.
+        </p>
+        <h3>Create a New React App</h3>
+        <p>
+          When starting a React project, a simple HTML page with script tags might still be the best
+          option. It only takes a minute to set up!
+        </p>
+        <p>
+          As your application grows, you might want to consider a more integrated setup. There are
+          several JavaScript toolchains we recommend for larger applications. Each of them can work
+          with little to no configuration and lets you take full advantage of the rich React
+          ecosystem. Learn how.
+        </p>{" "}
+        <h3>Try React</h3>
+        <p>
+          React has been designed from the start for gradual adoption, and you can use as little or
+          as much React as you need. Whether you want to get a taste of React, add some
+          interactivity to a simple HTML page, or start a complex React-powered app, the links in
+          this section will help you get started.
+        </p>
+        <h3>Online Playgrounds</h3>
+        <p>
+          If you’re interested in playing around with React, you can use an online code playground.
+          Try a Hello World template on CodePen, CodeSandbox, or Stackblitz.
+        </p>
+        <p>
+          If you prefer to use your own text editor, you can also download this HTML file, edit it,
+          and open it from the local filesystem in your browser. It does a slow runtime code
+          transformation, so we’d only recommend using this for simple demos.
+        </p>
+        <h3>Add React to a Website</h3>
+        <p>
+          You can add React to an HTML page in one minute. You can then either gradually expand its
+          presence, or keep it contained to a few dynamic widgets.
+        </p>
+        <h3>Create a New React App</h3>
+        <p>
+          When starting a React project, a simple HTML page with script tags might still be the best
+          option. It only takes a minute to set up!
+        </p>
+        <p>
+          As your application grows, you might want to consider a more integrated setup. There are
+          several JavaScript toolchains we recommend for larger applications. Each of them can work
+          with little to no configuration and lets you take full advantage of the rich React
+          ecosystem. Learn how.
+        </p>{" "}
+        <h3>Try React</h3>
+        <p>
+          React has been designed from the start for gradual adoption, and you can use as little or
+          as much React as you need. Whether you want to get a taste of React, add some
+          interactivity to a simple HTML page, or start a complex React-powered app, the links in
+          this section will help you get started.
+        </p>
+        <h3>Online Playgrounds</h3>
+        <p>
+          If you’re interested in playing around with React, you can use an online code playground.
+          Try a Hello World template on CodePen, CodeSandbox, or Stackblitz.
+        </p>
+        <p>
+          If you prefer to use your own text editor, you can also download this HTML file, edit it,
+          and open it from the local filesystem in your browser. It does a slow runtime code
+          transformation, so we’d only recommend using this for simple demos.
+        </p>
+        <h3>Add React to a Website</h3>
+        <p>
+          You can add React to an HTML page in one minute. You can then either gradually expand its
+          presence, or keep it contained to a few dynamic widgets.
+        </p>
+        <h3>Create a New React App</h3>
+        <p>
+          When starting a React project, a simple HTML page with script tags might still be the best
+          option. It only takes a minute to set up!
+        </p>
+        <p>
+          As your application grows, you might want to consider a more integrated setup. There are
+          several JavaScript toolchains we recommend for larger applications. Each of them can work
+          with little to no configuration and lets you take full advantage of the rich React
+          ecosystem. Learn how.
+        </p>{" "}
+        <h3>Try React</h3>
+        <p>
+          React has been designed from the start for gradual adoption, and you can use as little or
+          as much React as you need. Whether you want to get a taste of React, add some
+          interactivity to a simple HTML page, or start a complex React-powered app, the links in
+          this section will help you get started.
+        </p>
+        <h3>Online Playgrounds</h3>
+        <p>
+          If you’re interested in playing around with React, you can use an online code playground.
+          Try a Hello World template on CodePen, CodeSandbox, or Stackblitz.
+        </p>
+        <p>
+          If you prefer to use your own text editor, you can also download this HTML file, edit it,
+          and open it from the local filesystem in your browser. It does a slow runtime code
+          transformation, so we’d only recommend using this for simple demos.
+        </p>
+        <h3>Add React to a Website</h3>
+        <p>
+          You can add React to an HTML page in one minute. You can then either gradually expand its
+          presence, or keep it contained to a few dynamic widgets.
+        </p>
+        <h3>Create a New React App</h3>
+        <p>
+          When starting a React project, a simple HTML page with script tags might still be the best
+          option. It only takes a minute to set up!
+        </p>
+        <p>
+          As your application grows, you might want to consider a more integrated setup. There are
+          several JavaScript toolchains we recommend for larger applications. Each of them can work
+          with little to no configuration and lets you take full advantage of the rich React
+          ecosystem. Learn how.
+        </p>{" "}
+      </div>
+    </>
+  ) : null
+})
--- src/pages/best/hooks/useModal.tsx
+++ src/pages/best/hooks/useModal.tsx
@@ -1,10 +1,79 @@
-import { useEffect, useMemo, useRef, useState } from "react"
+import { RefObject, useCallback, useEffect, useMemo, useRef, useState } from "react"
 
 import { ModalBase } from "./BaseDialog"
 
-export const useModal = () => {
-  const [isOpen, setIsOpen] = useState(false)
-  const ref = useRef<HTMLElement>(null)
+// export const useModal = () => {
+//   const [isOpen, setIsOpen] = useState(false)
+//   const ref = useRef<HTMLElement>(null)
+//   const [scroll, setScroll] = useState(0)
+
+//   useEffect(() => {
+//     const element = ref.current
+//     if (!element) return
+
+//     const handleScroll = () => {
+//       setScroll(element?.scrollTop || 0)
+//     }
+
+//     element.addEventListener("scroll", handleScroll)
+//     return () => {
+//       element.removeEventListener("scroll", handleScroll)
+//     }
+//   })
+
+//   const open = () => setIsOpen(true)
+//   const close = () => setIsOpen(false)
+//   // const Dialog = () => <ModalBase onClosed={close} isOpen={isOpen} ref={ref} />
+//   const Dialog = useMemo(() => {
+//     return () => <ModalBase onClosed={close} isOpen={isOpen} ref={ref} />
+//   }, [isOpen])
+
+//   return { isOpen, Dialog, open, close }
+// }
+/* 返回记忆化的函数,状态 */
+// export const useModal = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+//   const ref = useRef<HTMLElement>(null);
+//   const [scroll, setScroll] = useState(0);
+
+//   const open = useCallback(() => {
+//     setIsOpen(true);
+//   }, []);
+
+//   const close = useCallback(() => {
+//     setIsOpen(false);
+//   }, []);
+
+//   useEffect(() => {
+//     const element = ref.current;
+//     if (!element) return;
+
+//     const handleScroll = () => {
+//       setScroll(element?.scrollTop || 0);
+//     };
+
+//     element.addEventListener("scroll", handleScroll);
+//     return () => {
+//       element.removeEventListener("scroll", handleScroll);
+//     };
+//   });
+
+//   const Dialog = useMemo(() => {
+//     return () => <ModalBase onClosed={close} isOpen={isOpen} ref={ref} />;
+//   }, [isOpen, close]);
+
+//   return useMemo(
+//     () => ({
+//       isOpen,
+//       Dialog,
+//       open,
+//       close
+//     }),
+//     [isOpen, Dialog, open, close]
+//   );
+// };
+/* 抽取出useScroll也无效 */
+const useScroll = (ref: RefObject) => {
   const [scroll, setScroll] = useState(0)
 
   useEffect(() => {
@@ -21,12 +90,33 @@ export const useModal = () => {
     }
   })
 
-  const open = () => setIsOpen(true)
-  const close = () => setIsOpen(false)
-  // const Dialog = () => <ModalBase onClosed={close} isOpen={isOpen} ref={ref} />
+  return scroll
+}
+
+export const useModal = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const ref = useRef<HTMLElement>(null)
+  const scroll = useScroll(ref)
+
+  const open = useCallback(() => {
+    setIsOpen(true)
+  }, [])
+
+  const close = useCallback(() => {
+    setIsOpen(false)
+  }, [])
+
   const Dialog = useMemo(() => {
     return () => <ModalBase onClosed={close} isOpen={isOpen} ref={ref} />
-  }, [isOpen])
+  }, [isOpen, close])
 
-  return { isOpen, Dialog, open, close }
+  return useMemo(
+    () => ({
+      isOpen,
+      Dialog,
+      open,
+      close,
+    }),
+    [isOpen, Dialog, open, close]
+  )
 }
