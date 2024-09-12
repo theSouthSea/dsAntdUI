e18ef844d05095cb4651f865bfc16cdcc6af86b2
dylan <1611185386@qq.com>
2024-09-12 09:05:58 +0800

    feat: 设计模式-策略模式在获取下拉框选项,显示列表数据反面的使用

--- src/pages/designPattern/Strategy/strategy.ts
+++ src/pages/designPattern/Strategy/strategy.ts
@@ -1,5 +1,24 @@
 // 维护一个规则的map
-const ruleMap = {
+// type RuleMap = {
+//   [key: string]: (name: string, v: string) => string | undefined
+// } & {
+//   minLength: (name: string, v: string, len: number) => string | undefined
+//   maxLength: (name: string, v: string, len: number) => string | undefined
+// }
+interface RuleMap {
+  minLength: (name: string, v: string, len: number) => string | undefined
+  maxLength: (name: string, v: string, len: number) => string | undefined
+  [key: string]: (name: string, v: any, len: number) => string | undefined
+}
+// type ITwoParams = {
+//   [key: string]: (name: string, v: any) => string | undefined
+// }
+// interface RuleMap extends ITwoParams{
+//   minLength: (name: string, v: string, len: number) => string | undefined
+//   maxLength: (name: string, v: string, len: number) => string | undefined
+
+// }
+const ruleMap: RuleMap = {
   isRequired: (name, v) => {
     if (v === undefined) {
       return `${name}是必需的`
@@ -10,39 +29,43 @@ const ruleMap = {
       return `${name}不能为空`
     }
   },
+  phone: (name, v) => {
+    if (!/(^1[3|5|8|7|9][0-9]{9}$)/.test(v)) {
+      return `${name}不是一个手机号`
+    }
+  },
   minLength: (name, v, len) => {
     if (v.length < len) {
       return `${name}不能少于${len}位`
     }
   },
   maxLength: (name, v, len) => {
-    if (v.length < len) {
+    if (v.length > len) {
       return `${name}不能多于${len}位`
     }
   },
-  phone: (name, v) => {
-    if (!/(^1[3|5|8|7|9][0-9]{9}$)/.test(v)) {
-      return `${name}不是一个手机号`
-    }
-  },
 }
-
+interface AddPramas {
+  name: string
+  field: string
+  validator: string
+}
 class Validator {
   // 记录校验结果
-  result = []
-
-  constructor(formData) {
+  result = [] as (string | undefined)[]
+  formData: Record<string, any>
+  constructor(formData: Record<string, any>) {
     this.formData = formData
   }
 
-  add({ name, field, validator }) {
+  add({ name, field, validator }: AddPramas) {
     const formItem = this.formData[field]
     console.log("name-field-value=", name, field, formItem)
     console.log("name-validator=", name, validator)
     // 区分length与其他规则
     if (validator.includes("Length")) {
       const va = validator.split(":")
-      this.result.push(ruleMap[va[0]](name, formItem, va[1]))
+      this.result.push(ruleMap[va[0]](name, formItem, Number(va[1])) as string)
     } else {
       this.result.push(ruleMap[validator](name, formItem))
     }
