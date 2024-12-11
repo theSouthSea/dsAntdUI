b00c4efe084a09bb093a08f5cfa9f952b7702f85
dylan <1611185386@qq.com>
2024-12-11 14:23:31 +0800

    feat: mobx与高级组件一起使用导致页面不更新的问题2

--- src/pages/stateLib/mobx/demo/HOCMobx/DataDisplay.tsx
+++ src/pages/stateLib/mobx/demo/HOCMobx/DataDisplay.tsx
@@ -6,13 +6,15 @@ import withData from "./withData"
 
 const DataDisplay = ({ data, value: propValue, onChange }) => {
   const [value, setValue] = useState(propValue)
-  const debounceChangeRef = useRef(debounce(onChange, 500))
+  // const debounceChangeRef = useRef(debounce(onChange, 500))
+  const debounceCallback = useCallback(debounce(onChange, 500), [])
   useEffect(() => {
     setValue(propValue)
   }, [propValue])
   const handleChange = useCallback((e) => {
     setValue(e.target.value)
-    debounceChangeRef.current(e)
+    // debounceChangeRef.current(e)
+    debounceCallback(e)
   }, [])
 
   if (!data) {
